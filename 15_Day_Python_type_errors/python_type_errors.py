61ff185b37c984f0e3454a4c81ad296faac4e563
NotJ0S3 <josemospina567@gmail.com>
2024-12-02 11:56:56 +0100

    Day 15 Done

---
+++ 15_Day_Python_type_errors/python_type_errors.py
@@ -0,0 +1,33 @@
+# Open you python interactive shell and try all the examples covered in this section.
+
+# SyntaxError:
+# print 'hello world'
+
+# NameError:
+# print(age)
+
+# IndexError:
+numbers = [1, 2, 3, 4, 5]
+print(numbers[5])
+
+# ModuleNotFoundError
+# import maths
+
+# AttributeError:
+# print(math.PI)
+
+# KeyError:
+users = {'name':'Asab', 'age':250, 'country':'Finland'}
+print(users['county'])
+
+# TyperError:
+print(4 + '3')
+
+# ImportError:
+from math import power
+
+# ValueError:
+# print(int(12a))
+
+# ZeroDivisionError:
+print(1/0)
